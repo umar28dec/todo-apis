07ef62aefed04006bae5f30ee56268f3e96080cb
Umar Farooque Khan <uffarooque73@gmail.com>
2025-06-02 19:56:08 +0530

    Refactor code style and improve formatting in Docker Compose and app.js files

--- nodejs-todo-api/src/app.js
+++ nodejs-todo-api/src/app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const todoRoutes = require('./routes/todoRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/api/todos', todoRoutes());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Node.js Todo API!");
+});
+
+app.use(todoRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
